e5f106b3146b3d2ca50f5d7dd1e7f77651ded4b2
Roberto Di Remigio <roberto.diremigio@gmail.com>
2017-08-28 16:20:08 +0200

    Add example of shared library

---
+++ recipe-0003/fortran-example/message.f90
@@ -0,0 +1,25 @@
+module messaging
+
+implicit none
+
+public Messenger
+type Messenger
+  character(len=19) :: message_
+end type
+
+public print_message
+
+private
+
+contains
+
+pure function print_message(postman) result(m)
+
+  type(Messenger), intent(in) :: postman
+  character(len=19) :: m
+
+  m = postman%message_
+
+end function
+
+end module
