1eac2fc1878820e82ad3c565d08307c7bfc9506f
Roberto Di Remigio <roberto.diremigio@gmail.com>
2017-09-27 03:06:22 +0200

    Add C++ example with MPI

---
+++ Chapter03/recipe-0016/cxx-example/hello-mpi.cpp
@@ -0,0 +1,40 @@
+// Author: Wes Kendall
+// Copyright 2011 www.mpitutorial.com
+// This code is provided freely with the tutorials on mpitutorial.com. Feel
+// free to modify it for your own use. Any distribution of the code must
+// either provide a link to www.mpitutorial.com or keep this header intact.
+//
+// An intro MPI hello world program that uses MPI_Init, MPI_Comm_size,
+// MPI_Comm_rank, MPI_Finalize, and MPI_Get_processor_name.
+//
+
+#include <iostream>
+
+#include <mpi.h>
+
+int main(int argc, char ** argv) {
+  // Initialize the MPI environment. The two arguments to MPI Init are not
+  // currently used by MPI implementations, but are there in case future
+  // implementations might need the arguments.
+  MPI_Init(NULL, NULL);
+
+  // Get the number of processes
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  // Get the rank of the process
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  // Get the name of the processor
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
+  // Print off a hello world message
+  std::cout << "Hello world from processor " << processor_name << ", rank  "
+            << world_rank << " out of " << world_size << " processors" << std::endl;
+
+  // Finalize the MPI environment. No more MPI calls can be made after this
+  MPI_Finalize();
+}
