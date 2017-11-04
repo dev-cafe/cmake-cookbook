fe1054fc2e1f3f5916db1345e0d89b87ea147a9f
Radovan Bast <bast@users.noreply.github.com>
2017-11-04 17:45:47 +0100

    do not capture an error from the calls
    my understanding is that we do not wish this script to fail
    and stop the CI pipeline ever but possibly I misunderstood the point

--- .scripts/report_versions.sh
+++ .scripts/report_versions.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# The return code will capture an error from ANY of the functions in the pipe
-set -euo pipefail
-
 echo "Report versions of whole tool stack"
 
 for tool in cmake make ninja; do
