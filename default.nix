7dfa19c2126eefd3907c54daa456d3b6cacaa195
Roberto Di Remigio <roberto.diremigio@gmail.com>
2017-11-17 23:09:12 +0100

    Update default.nix and .envrc

--- default.nix
+++ default.nix
@@ -1,33 +1,47 @@
-with import <nixpkgs> {}; {
-  cmakeEnv = stdenv.mkDerivation {
-    name = "cmake-recipes";
+let
+  hostPkgs = import <nixpkgs> {};
+  nixpkgs = (hostPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    rev = "ac355040656de04f59406ba2380a96f4124ebdad";
+    sha256 = "0frhc7mnx88sird6ipp6578k5badibsl0jfa22ab9w6qrb88j825";
+  });
+in
+  with import nixpkgs {
+    overlays = [(self: super:
+    {
+      atlas = super.atlas.override {
+        withLapack = true;
+      };
+      hdf5 = super.hdf5.override {
+        gfortran = super.gfortran;
+        mpi = super.openmpi;
+      };
+    }
+    )];
+  };
+  stdenv.mkDerivation {
+    name = "CMake-recipes";
     buildInputs = [
-      atlasWithLapack
+      atlas
       boost
-      bundler
       ccache
       clang
       clang-tools
       clang-analyzer
       cmake
       doxygen
+      exa
       eigen3_3
       gcc
       gfortran
-      gdb
-      hdf5-fortran
+      hdf5
       libuuid
-      lldb
       openmpi
-      python35Packages.breathe
-      python35Packages.jupyter
       python35Packages.matplotlib
       python35Packages.numpy
       python35Packages.pyyaml
-      python35Packages.scipy
-      python35Packages.sphinx
-      python35Packages.sympy
+      valgrind
       zlib
     ];
-  };
-}
+  }
