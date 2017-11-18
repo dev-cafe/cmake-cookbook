c9e05bc19ea52b69295c26fd8c547369fab3c124
Radovan Bast <bast@users.noreply.github.com>
2017-11-18 13:59:17 +0100

    show how to use args

--- Chapter12/recipe-01/cxx-example/test.py
+++ Chapter12/recipe-01/cxx-example/test.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 
 import subprocess
+import argparse
 
-# [1, 2, ..., 100]
-# we collect these as a list of strings
-one_to_hundred = [str(i) for i in range(1, 101)]
+# this test script expects an argument
+parser = argparse.ArgumentParser()
+parser.add_argument('--short',
+                    default=False,
+                    action='store_true',
+                    help='run the short test')
+parser.add_argument('--long',
+                    default=False,
+                    action='store_true',
+                    help='run the long test')
+args = parser.parse_args()
 
-output = subprocess.check_output(['./sum_up'] + one_to_hundred)
-output = int(output)
 
-assert output == 5050
+def execute_cpp_code(integers):
+    result = subprocess.check_output(['./sum_up'] + integers)
+    return int(result)
+
+
+if args.short:
+    # we collect [1, 2, ..., 100] as a list of strings
+    result = execute_cpp_code([str(i) for i in range(1, 101)])
+    assert result == 5050
+
+if args.long:
+    # we collect [1, 2, ..., 1000] as a list of strings
+    result = execute_cpp_code([str(i) for i in range(1, 1001)])
+    assert result == 500500
