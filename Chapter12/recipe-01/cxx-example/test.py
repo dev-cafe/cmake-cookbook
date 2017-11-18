8b01ba58c41e190b282083e3ab79dbb604da41e6
Radovan Bast <bast@users.noreply.github.com>
2017-11-18 13:32:22 +0100

    rename since these are strings and not integers

--- Chapter12/recipe-01/cxx-example/test.py
+++ Chapter12/recipe-01/cxx-example/test.py
@@ -4,9 +4,9 @@ import subprocess
 
 # [1, 2, ..., 100]
 # we collect these as a list of strings
-integers = [str(i) for i in range(1, 101)]
+one_to_hundred = [str(i) for i in range(1, 101)]
 
-output = subprocess.check_output(['./sum_up'] + integers)
+output = subprocess.check_output(['./sum_up'] + one_to_hundred)
 output = int(output)
 
 assert output == 5050
