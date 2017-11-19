9c29f8c400ea1884e2098ebd9178a9f1770e1fd7
Radovan Bast <bast@users.noreply.github.com>
2017-11-19 14:09:30 +0100

    add assert error messages
    to be used when we demonstrate how to inspect failed tests

--- Chapter12/recipe-01/cxx-example/test.py
+++ Chapter12/recipe-01/cxx-example/test.py
@@ -20,8 +20,8 @@ def execute_cpp_code(integers):
 if args.short:
     # we collect [1, 2, ..., 100] as a list of strings
     result = execute_cpp_code([str(i) for i in range(1, 101)])
-    assert result == 5050
+    assert result == 5050, 'summing up to 100 failed'
 else:
     # we collect [1, 2, ..., 1000] as a list of strings
     result = execute_cpp_code([str(i) for i in range(1, 1001)])
-    assert result == 500500
+    assert result == 500500, 'summing up to 1000 failed'
