92a4701c63e78b44c512f418c3652bddd447c8fb
Radovan Bast <bast@users.noreply.github.com>
2017-12-04 18:17:56 +0100

    show how to Record the Git hash at build-time

---
+++ Chapter06/recipe-07/cxx-example/example.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+// provides GIT_HASH
+#include "version.h"
+
+int main() {
+  std::cout << "This code has been built from version "
+            << GIT_HASH
+            << std::endl;
+}
---
+++ Chapter06/recipe-07/cxx-example/git-hash.cmake
@@ -0,0 +1,22 @@
+# in case Git is not available, we default to "unknown"
+set(GIT_HASH "unknown")
+
+# find Git and if available set GIT_HASH variable
+find_package(Git)
+if(GIT_FOUND)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} --no-pager show -s --pretty=format:%h -n 1
+    OUTPUT_VARIABLE GIT_HASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET
+    )
+endif()
+
+message(STATUS "Git hash is ${GIT_HASH}")
+
+# generate file version.h based on version.h.in
+configure_file(
+  ${CMAKE_CURRENT_LIST_DIR}/version.h.in
+  generated/version.h
+  @ONLY
+  )
