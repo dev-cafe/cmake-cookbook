7d30086beb695dfb03f9c1704396c218fe634ac7
Radovan Bast <bast@users.noreply.github.com>
2017-12-21 11:20:02 +0100

    recipe 3 replaces recipe 2

---
+++ Chapter06/recipe-02/cxx-example/generate.py
@@ -0,0 +1,26 @@
+"""
+Generates C++ vector of prime numbers up to max_number
+using sieve of Eratosthenes.
+"""
+import sys
+
+# for simplicity we do not verify argument list
+max_number = int(sys.argv[-2])
+output_file_name = sys.argv[-1]
+
+numbers = range(2, max_number + 1)
+is_prime = {number: True for number in numbers}
+
+for number in numbers:
+    current_position = number
+    if is_prime[current_position]:
+        while current_position <= max_number:
+            current_position += number
+            is_prime[current_position] = False
+
+primes = (number for number in numbers if is_prime[number])
+with open(output_file_name, 'w') as f:
+    f.write('const int max_number = {0};\n'.format(max_number))
+    f.write('std::vector<int> primes;\n')
+    for number in primes:
+        f.write('primes.push_back({0});\n'.format(number))
