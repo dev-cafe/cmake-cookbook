66a8f5fd3fba4d547cf1eec953c09cb4d577dff1
Radovan Bast <bast@users.noreply.github.com>
2017-12-30 20:10:31 +0100

    add boost.python recipe

---
+++ Chapter09/recipe-04/cxx-example/account.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <boost/python.hpp>
+
+class Account {
+public:
+  Account();
+  ~Account();
+
+  void deposit(const double amount);
+  void withdraw(const double amount);
+  double get_balance() const;
+
+private:
+  double balance;
+};
+
+namespace py = boost::python;
+
+BOOST_PYTHON_MODULE(account) {
+  py::class_<Account>("Account")
+      .def("deposit", &Account::deposit)
+      .def("withdraw", &Account::withdraw)
+      .def("get_balance", &Account::get_balance);
+}
---
+++ Chapter09/recipe-04/cxx-example/test.py
@@ -0,0 +1,16 @@
+from account import Account
+
+account1 = Account()
+
+account1.deposit(100.0)
+account1.deposit(100.0)
+
+account2 = Account()
+
+account2.deposit(200.0)
+account2.deposit(200.0)
+
+account1.withdraw(50.0)
+
+assert account1.get_balance() == 150.0
+assert account2.get_balance() == 400.0
