64b6094ecfaa84970c9495ab6689a1eb8c2c3345
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-01-21 14:42:18 +0100

    Wrap up and clean up

--- Chapter07/recipe-03/cxx-example/boost_cleanup.cmake
+++
@@ -1,8 +0,0 @@
-# Clean-up
-add_custom_command(
-    OUTPUT ${CUSTOM_BOOST_LOCATION}/boost.cleanedup
-    COMMAND ${CMAKE_COMMAND} -E remove_directory ${BOOST_BUILD_DIR}
-    COMMAND ${CMAKE_COMMAND} -E touch ${CUSTOM_BOOST_LOCATION}/boost.cleanedup
-    WORKING_DIRECTORY ${CUSTOM_BOOST_LOCATION}
-    DEPENDS ${CUSTOM_BOOST_LOCATION}/boost.installed
-    COMMENT "Clean-up Boost")
