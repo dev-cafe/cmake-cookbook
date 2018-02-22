6a5621ca795c743decb71cb86de50a4e49b01c15
Radovan Bast <bast@users.noreply.github.com>
2018-02-22 17:21:07 +0100

    disentangle cmake from rest of brew installations

--- .scripts/install_deps.sh
+++ .scripts/install_deps.sh
@@ -3,6 +3,17 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euo pipefail
 
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  brew update &> /dev/null
+  brew cask uninstall --force oclint
+  brew uninstall --force --ignore-dependencies boost
+  brew install gcc mpich pkg-config ossp-uuid python3 pyenv-virtualenv doxygen
+  brew install boost@1.59
+  brew install boost-python@1.59
+  # Symlink the installed Boost.Python to where all the rest of Boost resides
+  ln -sf /usr/local/opt/boost-python@1.59/lib/* /usr/local/opt/boost@1.59/lib
+fi
+
 CMake_VERSION="3.10.2"
 # OS-dependent operations
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
@@ -19,15 +30,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   fi
   echo "-- Done with CMake"
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  brew update &> /dev/null
-  brew cask uninstall --force oclint
-  brew uninstall --force --ignore-dependencies boost
-  brew install gcc mpich pkg-config ossp-uuid python3 pyenv-virtualenv doxygen
   brew upgrade cmake
-  brew install boost@1.59
-  brew install boost-python@1.59
-  # Symlink the installed Boost.Python to where all the rest of Boost resides
-  ln -sf /usr/local/opt/boost-python@1.59/lib/* /usr/local/opt/boost@1.59/lib
 fi
 
 echo "-- Installing Ninja"
