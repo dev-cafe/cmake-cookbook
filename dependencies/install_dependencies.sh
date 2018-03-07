e3519d9093b75edf1e129b6b12b1ad00030e8ee6
Roberto Di Remigio <robertodr@users.noreply.github.com>
2018-03-07 10:52:43 +0100

    Cache anaconda on Travis (#263)
    * Cache anaconda on Travis
    
    * Fix problem with existing directory
    
    * Some plumbing
    
    * Clean up

--- dependencies/install_dependencies.sh
+++ dependencies/install_dependencies.sh
@@ -90,16 +90,26 @@ else
 fi
 echo "-- Done with HDF5 $HDF5_VERSION"
 
-cd $HOME/Downloads
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  curl -Ls https://repo.continuum.io/archive/Anaconda2-5.1.0-Linux-x86_64.sh > conda.sh
-elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  curl -Ls https://repo.continuum.io/archive/Anaconda3-5.1.0-MacOSX-x86_64.sh > conda.sh
+Anaconda_VERSION="5.1.0"
+echo "-- Installing Anaconda $Anaconda_VERSION"
+if [[ -d $HOME/Deps/conda/bin ]]; then
+  echo "-- Anaconda $Anaconda_VERSION FOUND in cache"
+else
+  cd $HOME/Downloads
+  echo "-- Anaconda $Anaconda_VERSION NOT FOUND in cache"
+  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    curl -Ls https://repo.continuum.io/archive/Anaconda2-${Anaconda_VERSION}-Linux-x86_64.sh > conda.sh
+  elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    curl -Ls https://repo.continuum.io/archive/Anaconda3-${Anaconda_VERSION}-MacOSX-x86_64.sh > conda.sh
+  fi
+  # Travis creates the cached directories for us.
+  # This is problematic when wanting to install Anaconda for the first time...
+  rm -rf $HOME/Deps/conda
+  bash conda.sh -b -p $HOME/Deps/conda
+  PATH=$HOME/Deps/conda/bin${PATH:+:$PATH}
+  conda config --set always_yes yes --set changeps1 no
+  conda update -q conda
+  conda info -a
+  cd $TRAVIS_BUILD_DIR
 fi
-bash conda.sh -b -p $HOME/Deps/conda
-PATH=$HOME/Deps/conda/bin${PATH:+:$PATH}
-conda config --set always_yes yes --set changeps1 no
-conda update -q conda
-conda info -a
-cd $TRAVIS_BUILD_DIR
-echo "-- Done with Conda"
+echo "-- Done with Anaconda $Anaconda_VERSION"
