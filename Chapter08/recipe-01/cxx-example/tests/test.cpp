dc278eab40510a4b1287b8bfb3b5c4ce83224f91
Radovan Bast <bast@users.noreply.github.com>
2018-03-14 09:56:08 +0100

    draft recipe 1

---
+++ Chapter08/recipe-01/cxx-example/tests/test.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+#include "sum_integers.hpp"
+
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in
+                          // one cpp file
+#include "catch.hpp"
+
+TEST_CASE("Sum of integers for a short vector", "[short]") {
+  std::vector<int> integers = {1, 2, 3, 4, 5};
+  REQUIRE(sum_integers(integers) == 15);
+}
+
+TEST_CASE("Sum of integers for a longer vector", "[long]") {
+  std::vector<int> integers;
+  for (int i = 1; i < 1001; ++i) {
+    integers.push_back(i);
+  }
+  REQUIRE(sum_integers(integers) == 500500);
+}
