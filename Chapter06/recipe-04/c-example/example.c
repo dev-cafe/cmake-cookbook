9d92730bc366866aa3928963096a63eacca47799
Radovan Bast <bast@users.noreply.github.com>
2018-03-29 20:39:07 +0200

    better align fortran and c examples

--- Chapter06/recipe-04/c-example/example.c
+++ Chapter06/recipe-04/c-example/example.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-// provides PROJECT_VERSION
+// provides PROJECT_VERSION, PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR
 #include "version.h"
 
 int main() {
-  printf("This is output from example code v%s:\n", PROJECT_VERSION);
+  printf("This is output from example code %s\n", PROJECT_VERSION);
+  printf("The major version number is %i\n", PROJECT_VERSION_MAJOR);
+  printf("The minor version number is %i\n", PROJECT_VERSION_MINOR);
 
   printf("Hello CMake world!\n");
 }
