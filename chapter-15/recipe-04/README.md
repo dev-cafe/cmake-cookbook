141190d20dfcd771c635e0c3171d2f4feaf25e01
Radovan Bast <bast@users.noreply.github.com>
2018-04-08 14:38:46 +0200

    cdash recipe with tsan

---
+++ chapter-15/recipe-04/README.md
@@ -0,0 +1,5 @@
+# Using the ThreadSanitizer and reporting data races to CDash
+
+Abstract to be written ...
+
+- [cxx-example](cxx-example/)
---
+++ chapter-15/recipe-04/cxx-example/example.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+static const int num_threads = 16;
+
+void increase(int i, int &s) {
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "thread " << i << " increases " << s++ << std::endl;
+}
+
+int main() {
+    std::thread t[num_threads];
+
+    int s = 0;
+
+    // start threads
+    for (auto i = 0; i < num_threads; i++) {
+        t[i] = std::thread(increase, i, std::ref(s));
+    }
+
+    // join threads with main thread
+    for (auto i = 0; i < num_threads; i++) {
+        t[i].join();
+    }
+
+    std::cout << "final s: " << s << std::endl;
+
+    return 0;
+}
