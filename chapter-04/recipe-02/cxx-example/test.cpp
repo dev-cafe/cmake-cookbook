ba91aa14a909f64c58ced5a447e21ecd4373321f
Radovan Bast <bast@users.noreply.github.com>
2018-05-05 13:43:27 +0200

    reorder includes in chapter 4

--- chapter-04/recipe-02/cxx-example/test.cpp
+++ chapter-04/recipe-02/cxx-example/test.cpp
@@ -1,10 +1,10 @@
 #include "sum_integers.hpp"
 
-#include <vector>
-
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in
                           // one cpp file
 #include "catch.hpp"
+
+#include <vector>
 
 TEST_CASE("Sum of integers is computed", "[sum]") {
   auto integers = {1, 2, 3, 4, 5};
