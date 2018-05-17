4567cef31e50aafd3ca33997bc5c15351d1b8aa3
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-05-16 22:35:33 -0400

    Install ZeroMQ on Travis too

--- testing/dependencies/install_dependencies.sh
+++ testing/dependencies/install_dependencies.sh
@@ -10,14 +10,15 @@ elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   brew cask uninstall --force oclint
   brew uninstall --force --ignore-dependencies boost
   brew upgrade python
-  brew install pipenv
+  brew install boost-python@1.59
+  brew install boost@1.59
+  brew install doxygen
   brew install gcc
   brew install mpich
-  brew install pkg-config
   brew install ossp-uuid
-  brew install doxygen
-  brew install boost@1.59
-  brew install boost-python@1.59
+  brew install pipenv
+  brew install pkg-config
+  brew install zeromq
   # Symlink the installed Boost.Python to where all the rest of Boost resides
   ln -sf /usr/local/opt/boost-python@1.59/lib/* /usr/local/opt/boost@1.59/lib
 fi
