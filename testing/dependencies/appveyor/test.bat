edb42b9f6462d4eb58ec728ccd0a098d344ffff3
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-05-21 22:01:02 -0400

    Proper caching of MSYS2 packages

--- testing/dependencies/appveyor/test.bat
+++ testing/dependencies/appveyor/test.bat
@@ -7,4 +7,5 @@ rem bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run pyt
 rem bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-03/recipe-*'"
 rem bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-04/recipe-*'"
 rem bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-05/recipe-*'"
-bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-06/recipe-*'"
+rem bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-06/recipe-*'"
+bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-07/recipe-*'"
