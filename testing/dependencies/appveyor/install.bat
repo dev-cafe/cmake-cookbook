f43c3737a2f0307d160a35c2c8a968e0b074e98d
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-05-26 00:00:24 -0400

    Install pipenv

--- testing/dependencies/appveyor/install.bat
+++ testing/dependencies/appveyor/install.bat
@@ -14,9 +14,6 @@ if "%nonVSGenerator%"=="true" (
   rem --ask=127 is taken from https://github.com/appveyor/ci/issues/2074#issuecomment-364842018
   bash -lc "pacman -Syuu --needed --noconfirm --ask=127"
 
-  rem we will run the tests inside pipenv
-  bash -lc "pip install pipenv"
-
   rem more packages
   bash -lc "pacman -S --noconfirm mingw64/mingw-w64-x86_64-boost"
   bash -lc "pacman -S --noconfirm mingw64/mingw-w64-x86_64-ninja"
