67e6d10876c06a9e3eb614741b317ab59fd43af0
Radovan Bast <bast@users.noreply.github.com>
2018-05-26 18:31:57 +0200

    hopefully restore appveyor zip and extract; fixes #356

--- testing/dependencies/appveyor/test.bat
+++ testing/dependencies/appveyor/test.bat
@@ -1,9 +1,9 @@
 rem install pipenv dependencies
-bash -lc "cd /c/projects/cmake-recipes-no-symlinks && pipenv install"
+bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pipenv install"
 
 rem finally we start with the actual tests
-bash -lc "cd /c/projects/cmake-recipes-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-01/recipe-*'"
-bash -lc "cd /c/projects/cmake-recipes-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-02/recipe-*'"
-bash -lc "cd /c/projects/cmake-recipes-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-03/recipe-*'"
-bash -lc "cd /c/projects/cmake-recipes-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-04/recipe-*'"
-bash -lc "cd /c/projects/cmake-recipes-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-05/recipe-*'"
+bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-01/recipe-*'"
+bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-02/recipe-*'"
+bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-03/recipe-*'"
+bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-04/recipe-*'"
+bash -lc "cd /c/projects/cmake-cookbook-no-symlinks && pwd && pipenv run python testing/collect_tests.py 'chapter-05/recipe-*'"
