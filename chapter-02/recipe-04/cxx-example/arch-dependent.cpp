d26882e1edd0cfeb7595e1f72e0f6d35c91c5eab
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-05-27 23:41:59 -0400

    Rework recipe 4 to address reviewer's comment
    CMAKE_SIZEOF_VOID_P is the only cross-platform way to discriminate
    32 vs 64 bit archs and it will also work for cross-compilation.

--- chapter-02/recipe-04/cxx-example/arch-dependent.cpp
+++ chapter-02/recipe-04/cxx-example/arch-dependent.cpp
@@ -2,14 +2,18 @@
 #include <iostream>
 #include <string>
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
 std::string say_hello() {
+  std::string arch_info(TOSTRING(ARCHITECTURE));
+  arch_info += std::string(" architecture. ");
 #ifdef IS_32_BIT_ARCH
-  return std::string("this code is compiled on a 32 bit host processor");
+  return arch_info + std::string("This code is compiled on a 32 bit host processor");
 #elif IS_64_BIT_ARCH
-  return std::string("this code is compiled on a 64 bit host processor");
+  return arch_info + std::string("This code is compiled on a 64 bit host processor");
 #else
-  return std::string(
-      "this code is compiled on a host processor with unknown architecture");
+  return arch_info + std::string("Neither 32 nor 64 bit host processor, puzzling...");
 #endif
 }
 
