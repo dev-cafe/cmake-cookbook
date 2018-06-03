09745287f172cffdcab399fa2a1e56d5661277f4
Radovan Bast <bast@users.noreply.github.com>
2018-06-03 15:21:56 +0200

    reduce recipe to a configuration-only recipe
    I think in this case it does not make much sense to include
    source code.
    
    We now show only C and C++ but readers will be able to trivially
    adapt to their Fortran projects.

--- chapter-01/recipe-07/cxx-example/geometry_rhombus.cpp
+++
@@ -1,7 +0,0 @@
-#include "geometry_rhombus.hpp"
-
-namespace geometry {
-namespace area {
-double rhombus(double d1, double d2) { return (d1 * d2) / 2.0; }
-} // namespace area
-} // namespace geometry
--- chapter-01/recipe-07/cxx-example/geometry_square.cpp
+++
@@ -1,9 +0,0 @@
-#include "geometry_square.hpp"
-
-#include <cmath>
-
-namespace geometry {
-namespace area {
-double square(double l) { return std::pow(l, 2); }
-} // namespace area
-} // namespace geometry
