0ae405ac5f4043d36491f75a99fca8d1255f4059
Radovan Bast <bast@users.noreply.github.com>
2018-06-03 14:40:53 +0200

    symlink files which have not changed

--- chapter-01/recipe-05/cxx-example/Message.cpp
+++ chapter-01/recipe-05/cxx-example/Message.cpp
@@ -1,11 +1,1 @@
-#include "Message.hpp"
-
-#include <iostream>
-#include <string>
-
-std::ostream &Message::printObject(std::ostream &os) {
-  os << "This is my very nice message: " << std::endl;
-  os << message_;
-
-  return os;
-}
+../../recipe-03/cxx-example/Message.cpp
