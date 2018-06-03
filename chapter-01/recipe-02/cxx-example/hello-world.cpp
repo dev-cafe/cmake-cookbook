9141f21c5af6ab024f557097fab7562767b52e5f
Radovan Bast <bast@users.noreply.github.com>
2018-06-03 12:30:37 +0200

    symlink files which are identical to recipe 1

--- chapter-01/recipe-02/cxx-example/hello-world.cpp
+++ chapter-01/recipe-02/cxx-example/hello-world.cpp
@@ -1,10 +1,1 @@
-#include <cstdlib>
-#include <iostream>
-#include <string>
-
-std::string say_hello() { return std::string("Hello, CMake world!"); }
-
-int main() {
-  std::cout << say_hello() << std::endl;
-  return EXIT_SUCCESS;
-}
+../../recipe-01/cxx-example/hello-world.cpp
