5f844b759ac9529a0c0dbd6750cd0b6c493f617b
Radovan Bast <bast@users.noreply.github.com>
2018-06-05 14:37:06 +0200

    shorter line prints to avoid wrap

--- chapter-02/recipe-04/cxx-example/arch-dependent.cpp
+++ chapter-02/recipe-04/cxx-example/arch-dependent.cpp
@@ -9,11 +9,11 @@ std::string say_hello() {
   std::string arch_info(TOSTRING(ARCHITECTURE));
   arch_info += std::string(" architecture. ");
 #ifdef IS_32_BIT_ARCH
-  return arch_info + std::string("This code is compiled on a 32 bit host processor");
+  return arch_info + std::string("Compiled on a 32 bit host processor.");
 #elif IS_64_BIT_ARCH
-  return arch_info + std::string("This code is compiled on a 64 bit host processor");
+  return arch_info + std::string("Compiled on a 64 bit host processor.");
 #else
-  return arch_info + std::string("Neither 32 nor 64 bit host processor, puzzling...");
+  return arch_info + std::string("Neither 32 nor 64 bit, puzzling ...");
 #endif
 }
 
