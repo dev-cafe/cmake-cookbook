6470d19812cad558e5c3750034027bde70766978
Roberto Di Remigio <robertodr@users.noreply.github.com>
2018-06-05 22:19:08 -0400

    Fully qualify namespaces, use auto

--- chapter-02/recipe-06/cxx-example/linear-algebra.cpp
+++ chapter-02/recipe-06/cxx-example/linear-algebra.cpp
@@ -1,12 +1,10 @@
-#include <Eigen/Dense>
 #include <chrono>
 #include <iostream>
 
-using namespace Eigen;
-using namespace std;
+#include <Eigen/Dense>
 
 EIGEN_DONT_INLINE
-double simple_function(VectorXd &va, VectorXd &vb) {
+double simple_function(Eigen::VectorXd &va, Eigen::VectorXd &vb) {
   // this simple function computes the dot product of two vectors
   // of course it could be expressed more compactly
   double d = va.dot(vb);
@@ -17,22 +15,18 @@ int main() {
   int len = 1000000;
   int num_repetitions = 100;
 
-  std::chrono::time_point<std::chrono::system_clock> start, end;
-  std::chrono::duration<double> elapsed_seconds;
-  std::time_t end_time;
-
   // generate two random vectors
-  VectorXd va = VectorXd::Random(len);
-  VectorXd vb = VectorXd::Random(len);
+  Eigen::VectorXd va = Eigen::VectorXd::Random(len);
+  Eigen::VectorXd vb = Eigen::VectorXd::Random(len);
 
   double result;
-  start = std::chrono::system_clock::now();
+  auto start = std::chrono::system_clock::now();
   for (auto i = 0; i < num_repetitions; i++) {
     result = simple_function(va, vb);
   }
-  end = std::chrono::system_clock::now();
-  elapsed_seconds = end - start;
+  auto end = std::chrono::system_clock::now();
+  auto elapsed_seconds = end - start;
 
-  cout << "result: " << result << endl;
-  cout << "elapsed seconds: " << elapsed_seconds.count() << endl;
+  std::cout << "result: " << result << std::endl;
+  std::cout << "elapsed seconds: " << elapsed_seconds.count() << std::endl;
 }
