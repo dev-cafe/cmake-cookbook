ba1643b6e21b613626360675f314a1e11e452af5
Radovan Bast <bast@users.noreply.github.com>
2018-06-10 18:02:41 +0200

    cut a lot of code not needed for this recipe

--- chapter-03/recipe-04/cxx-example/CxxBLAS.hpp
+++ chapter-03/recipe-04/cxx-example/CxxBLAS.hpp
@@ -1,5 +1,4 @@
-// Resolve mangling of some BLAS subroutines
-// Adapted from Psi4
+// resolve mangling, adapted from Psi4
 
 #pragma once
 
@@ -9,21 +8,6 @@
 extern "C" {
 #endif
 
-#define F_DGEMM dgemm_
-extern void F_DGEMM(char *,
-                    char *,
-                    int *,
-                    int *,
-                    int *,
-                    double *,
-                    double *,
-                    int *,
-                    double *,
-                    int *,
-                    double *,
-                    double *,
-                    int *);
-
 #define F_DSCAL dscal_
 extern void F_DSCAL(int *n, double *alpha, double *vec, int *inc);
 
@@ -31,27 +15,5 @@ extern void F_DSCAL(int *n, double *alpha, double *vec, int *inc);
 }
 #endif
 
-void C_DGEMM(char transa,
-             char transb,
-             int m,
-             int n,
-             int k,
-             double alpha,
-             double *a,
-             int lda,
-             double *b,
-             int ldb,
-             double beta,
-             double *c,
-             int ldc);
-
-/*!
- * This function scales a vector by a real scalar.
- *
- * \param length length of array
- * \param alpha  scale factor
- * \param vec    vector to scale
- * \param inc    how many places to skip to get to next element in vec
- *
- */
+// scales a vector by a real scalar
 void C_DSCAL(size_t length, double alpha, double *vec, int inc);
--- chapter-03/recipe-04/cxx-example/CxxLAPACK.hpp
+++ chapter-03/recipe-04/cxx-example/CxxLAPACK.hpp
@@ -1,5 +1,4 @@
-// Resolve mangling of some LAPACK subroutines
-// Adapted from Psi4
+// resolve mangling, adapted from Psi4
 
 #pragma once
 
@@ -10,21 +9,8 @@ extern "C" {
 #define F_DGESV dgesv_
 extern int F_DGESV(int *, int *, double *, int *, int *, double *, int *, int *);
 
-#define F_DSYEV dsyev_
-extern int
-F_DSYEV(char *, char *, int *, double *, int *, double *, double *, int *, int *);
-
 #ifdef __cplusplus
 }
 #endif
 
 int C_DGESV(int n, int nrhs, double *a, int lda, int *ipiv, double *b, int ldb);
-
-int C_DSYEV(char jobz,
-            char uplo,
-            int n,
-            double *a,
-            int lda,
-            double *w,
-            double *work,
-            int lwork);
