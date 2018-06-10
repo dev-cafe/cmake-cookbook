a379b0236b3e1c02b887a7ce9f32cbadea57e75c
Radovan Bast <bast@users.noreply.github.com>
2018-06-10 18:09:10 +0200

    rm all the timing, it just clutters
    I don't thin the timing information code is needed here.
    This is not the point and it makes it harder to see the essence
    through the forest of code.

--- chapter-03/recipe-04/cxx-example/linear-algebra.cpp
+++ chapter-03/recipe-04/cxx-example/linear-algebra.cpp
@@ -4,7 +4,6 @@
 #include "CxxBLAS.hpp"
 #include "CxxLAPACK.hpp"
 
-#include <chrono>
 #include <cstdlib>
 #include <iostream>
 #include <random>
@@ -21,12 +20,7 @@ int main(int argc, char **argv) {
   std::mt19937 mt(rd());
   std::uniform_real_distribution<double> dist(-1.0, 1.0);
 
-  std::chrono::time_point<std::chrono::system_clock> start, end;
-  std::chrono::duration<double> elapsed_seconds;
-  std::time_t end_time;
-
   // Allocate matrices and right-hand side vector
-  start = std::chrono::system_clock::now();
   int dim = std::atoi(argv[1]);
   std::vector<double> A(dim * dim);
   std::vector<double> b(dim);
@@ -38,39 +32,23 @@ int main(int argc, char **argv) {
     }
     b[r] = dist(mt);
   }
-  end = std::chrono::system_clock::now();
-  // Report times
-  elapsed_seconds = end - start;
-  end_time = std::chrono::system_clock::to_time_t(end);
-  std::cout << "matrices allocated and initialized " << std::ctime(&end_time)
-            << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
-  start = std::chrono::system_clock::now();
   // Scale RHS vector by a random number between -1.0 and 1.0
   C_DSCAL(dim, dist(mt), b.data(), 1);
+  std::cout << "C_DSCAL done" << std::endl;
+
   // Save matrix and RHS
   std::vector<double> A1(A);
   std::vector<double> b1(b);
-  end = std::chrono::system_clock::now();
-  end_time = std::chrono::system_clock::to_time_t(end);
-  std::cout << "C_DSCAL done, A and b saved " << std::ctime(&end_time)
-            << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
   int info;
-  start = std::chrono::system_clock::now();
   info = C_DGESV(dim, 1, A.data(), dim, ipiv.data(), b.data(), dim);
-  end = std::chrono::system_clock::now();
-  // Report times
-  elapsed_seconds = end - start;
-  end_time = std::chrono::system_clock::to_time_t(end);
-
-  std::cout << "C_DGESV done " << std::ctime(&end_time)
-            << "elapsed time: " << elapsed_seconds.count() << "s\n";
+  std::cout << "C_DGESV done" << std::endl;
   std::cout << "info is " << info << std::endl;
 
-  double eps = 0.;
+  double eps = 0.0;
   for (int i = 0; i < dim; ++i) {
-    double sum = 0.;
+    double sum = 0.0;
     for (int j = 0; j < dim; ++j)
       sum += A1[i + j * dim] * b[j];
     eps += std::abs(b1[i] - sum);
