10455311c4783ec7ee6ff513db11c9574867a792
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-06-12 21:17:25 -0400

    Appease shellcheck

--- testing/dependencies/travis/anaconda.sh
+++ testing/dependencies/travis/anaconda.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -euxo pipefail
 
 Anaconda_VERSION="5.1.0"
 echo "-- Installing Anaconda $Anaconda_VERSION"
 if [[ -d "$HOME/Deps/conda/bin" ]]; then
   echo "-- Anaconda $Anaconda_VERSION FOUND in cache"
 else
-  cd $HOME/Downloads
+  cd "$HOME"/Downloads
   echo "-- Anaconda $Anaconda_VERSION NOT FOUND in cache"
   if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     curl -Ls https://repo.continuum.io/archive/Anaconda2-${Anaconda_VERSION}-Linux-x86_64.sh > conda.sh
@@ -16,13 +16,13 @@ else
   fi
   # Travis creates the cached directories for us.
   # This is problematic when wanting to install Anaconda for the first time...
-  rm -rf $HOME/Deps/conda
-  bash conda.sh -b -p $HOME/Deps/conda
+  rm -rf "$HOME"/Deps/conda
+  bash conda.sh -b -p "$HOME"/Deps/conda
   PATH=$HOME/Deps/conda/bin${PATH:+:$PATH}
   conda config --set always_yes yes --set changeps1 no
   conda update -q conda
   conda info -a
-  cd $TRAVIS_BUILD_DIR
-  rm -f $HOME/Downloads/conda.sh
+  cd "$TRAVIS_BUILD_DIR"
+  rm -f "$HOME"/Downloads/conda.sh
 fi
 echo "-- Done with Anaconda $Anaconda_VERSION"
