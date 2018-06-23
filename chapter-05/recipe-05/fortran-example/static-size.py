39a136c57b9005ca7271d54ef5c63ff4178c9e67
Radovan Bast <bast@users.noreply.github.com>
2018-06-23 14:48:12 +0200

    5/5 if "size" not available, print a nice msg but do not fail

--- chapter-05/recipe-05/fortran-example/static-size.py
+++ chapter-05/recipe-05/fortran-example/static-size.py
@@ -5,7 +5,11 @@ import subprocess
 # arguments and whether the file really exists
 file_path = sys.argv[-1]
 
-output = subprocess.check_output(['size', file_path]).decode('utf-8')
+try:
+  output = subprocess.check_output(['size', file_path]).decode('utf-8')
+except FileNotFoundError:
+  print('command "size" is not available on this platform')
+  sys.exit(0)
 
 for line in output.split('\n'):
     if file_path in line:
