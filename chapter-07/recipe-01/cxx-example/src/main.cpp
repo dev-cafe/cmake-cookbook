912279fdbc4507efa1dd34eed50f44ea8a1381a6
Radovan Bast <bast@users.noreply.github.com>
2018-06-26 14:59:29 +0200

    7/1 symlink files to avoid divergence

--- chapter-07/recipe-01/cxx-example/src/main.cpp
+++ chapter-07/recipe-01/cxx-example/src/main.cpp
@@ -1,17 +1,1 @@
-#include "sum_integers.hpp"
-
-#include <iostream>
-#include <vector>
-
-// we assume all arguments are integers and we sum them up
-// for simplicity we do not verify the type of arguments
-int main(int argc, char *argv[]) {
-
-  std::vector<int> integers;
-  for (int i = 1; i < argc; i++) {
-    integers.push_back(atoi(argv[i]));
-  }
-  int sum = sum_integers(integers);
-
-  std::cout << sum << std::endl;
-}
+../../../../chapter-04/recipe-01/cxx-example/main.cpp
