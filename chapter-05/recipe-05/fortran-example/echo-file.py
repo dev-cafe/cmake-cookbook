9fc32376b5b10c482040498e051030a0776f3606
Radovan Bast <bast@users.noreply.github.com>
2018-06-30 14:40:19 +0200

    do not crash if link.txt not found; fixes #396
    instead of crashing hard, the code now prints an error
    message that the file does not exist

--- chapter-05/recipe-05/fortran-example/echo-file.py
+++ chapter-05/recipe-05/fortran-example/echo-file.py
@@ -1,8 +1,11 @@
 import sys
 
-# for simplicity we do not check number of
-# arguments and whether the file really exists
+# for simplicity we do not verify the number and
+# type of arguments
 file_path = sys.argv[-1]
 
-with open(file_path, 'r') as f:
-    print(f.read())
+try:
+    with open(file_path, 'r') as f:
+        print(f.read())
+except FileNotFoundError:
+    print('ERROR: file {0} not found'.format(file_path))
