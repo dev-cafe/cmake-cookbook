1fa20575cae2bb69c1032bc41785e4f5af3997f5
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-07-18 11:52:40 -0400

    Install Miniconda and build tools

--- testing/dependencies/travis/anaconda.sh
+++ testing/dependencies/travis/anaconda.sh
@@ -2,27 +2,32 @@
 
 set -eu -o pipefail
 
-Anaconda_VERSION="5.2.0"
-echo "-- Installing Anaconda $Anaconda_VERSION"
+echo "-- Installing latest Miniconda"
 if [[ -d "$HOME/Deps/conda/bin" ]]; then
-  echo "-- Anaconda $Anaconda_VERSION FOUND in cache"
+    echo "-- Miniconda latest version FOUND in cache"
 else
-  cd "$HOME"/Downloads
-  echo "-- Anaconda $Anaconda_VERSION NOT FOUND in cache"
-  if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    curl -Ls https://repo.continuum.io/archive/Anaconda3-${Anaconda_VERSION}-Linux-x86_64.sh > conda.sh
-  elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    curl -Ls https://repo.continuum.io/archive/Anaconda3-${Anaconda_VERSION}-MacOSX-x86_64.sh > conda.sh
-  fi
-  # Travis creates the cached directories for us.
-  # This is problematic when wanting to install Anaconda for the first time...
-  rm -rf "$HOME"/Deps/conda
-  bash conda.sh -b -p "$HOME"/Deps/conda
-  PATH=$HOME/Deps/conda/bin${PATH:+:$PATH}
-  conda config --set always_yes yes --set changeps1 no
-  conda update -q conda
-  conda info -a
-  cd "$TRAVIS_BUILD_DIR"
-  rm -f "$HOME"/Downloads/conda.sh
+    cd "$HOME"/Downloads
+    echo "-- Miniconda latest version NOT FOUND in cache"
+    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+        curl -Ls https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh > miniconda.sh
+    elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+        curl -Ls https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh > miniconda.sh
+    fi
+    # Travis creates the cached directories for us.
+    # This is problematic when wanting to install Anaconda for the first time...
+    rm -rf "$HOME"/Deps/conda
+    bash miniconda.sh -b -p "$HOME"/Deps/conda &> /dev/null
+    touch "$HOME"/Deps/conda/conda-meta/pinned
+    PATH=$HOME/Deps/conda/bin${PATH:+:$PATH}
+    conda config --set show_channel_urls True &> /dev/null
+    conda config --set always_yes yes &> /dev/null
+    conda config --set changeps1 no &> /dev/null
+    conda update --all --yes &> /dev/null
+    conda clean -tipy &> /dev/null
+    # Install conda build and deployment tools.
+    conda install --yes --quiet conda-build anaconda-client jinja2 setuptools &> /dev/null
+    conda clean -tipsy &> /dev/null
+    cd "$TRAVIS_BUILD_DIR"
+    rm -f "$HOME"/Downloads/miniconda.sh
 fi
-echo "-- Done with Anaconda $Anaconda_VERSION"
+echo "-- Done with latest Miniconda"
