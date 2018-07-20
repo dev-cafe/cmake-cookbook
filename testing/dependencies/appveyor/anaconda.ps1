d96c12986ebb1998c9ab49d40af46d209c75c6f9
Roberto Di Remigio <robertodr@users.noreply.github.com>
2018-07-20 02:58:33 -0400

    Install Miniconda and build tools rather than full Anaconda (#413)
    Install Miniconda and build tools rather than full Anaconda

--- testing/dependencies/appveyor/anaconda.ps1
+++ testing/dependencies/appveyor/anaconda.ps1
@@ -3,10 +3,10 @@
 # License: CC0 1.0 Universal: http://creativecommons.org/publicdomain/zero/1.0/
 # Modified by Roberto Di Remigio to install Anaconda
 
-function DownloadAnaconda ($Anaconda_VERSION) {
+function DownloadAnaconda () {
     $webclient = New-Object System.Net.WebClient
-    $filename = "Anaconda3-" + $Anaconda_VERSION + "-Windows-x86_64.exe"
-    $url = "https://repo.anaconda.com/archive/" + $filename
+    $filename = "Miniconda3-latest-Windows-x86_64.exe"
+    $url = "https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe"
 
     $basedir = $pwd.Path + "\"
     $filepath = $basedir + $filename
@@ -36,22 +36,22 @@ function DownloadAnaconda ($Anaconda_VERSION) {
    return $filepath
 }
 
-function InstallAnaconda ($Anaconda_VERSION, $Anaconda_cache) {
-    Write-Host "Installing Anaconda" $Anaconda_VERSION
+function InstallAnaconda ($Anaconda_cache) {
+    Write-Host "Installing latest Miniconda"
     if (Test-Path $Anaconda_cache) {
-        Write-Host "-- Anaconda " $Anaconda_VERSION "FOUND in cache"
+        Write-Host "-- Miniconda latest version FOUND in cache"
         return $false
     }
 
-    Write-Host "-- Anaconda" $Anaconda_VERSION "NOT FOUND in cache"
-    $filepath = DownloadAnaconda $Anaconda_VERSION
+    Write-Host "-- Miniconda latest version NOT FOUND in cache"
+    $filepath = DownloadAnaconda
     Write-Host "Installing" $filepath "to" $Anaconda_cache
     $install_log = $Anaconda_cache + ".log"
     $args = "/InstallationType=JustMe /AddToPath=0 /RegisterPython=0 /S /D=$Anaconda_cache"
     Write-Host $filepath $args
     Start-Process -FilePath $filepath -ArgumentList $args -Wait -Passthru
     if (Test-Path $Anaconda_cache) {
-        Write-Host "Anaconda $Anaconda_VERSION installation complete"
+        Write-Host "Miniconda latest version installation complete"
     } else {
         Write-Host "Failed to install Python in $Anaconda_cache"
         Get-Content -Path $install_log
@@ -63,11 +63,31 @@ function SetUpConda ($Anaconda_cache) {
     $conda_path = $Anaconda_cache + "\Scripts\conda.exe"
     Write-Host "Setting up conda..."
 
-    $args = "config --set always_yes yes --set changeps1 no"
+    $args = "config --set show_channel_urls True"
     Write-Host $conda_path $args
     Start-Process -FilePath "$conda_path" -ArgumentList $args -Wait -Passthru
 
-    $args = "update -q conda"
+    $args = "config --set always_yes yes"
+    Write-Host $conda_path $args
+    Start-Process -FilePath "$conda_path" -ArgumentList $args -Wait -Passthru
+
+    $args = "config --set changeps1 no"
+    Write-Host $conda_path $args
+    Start-Process -FilePath "$conda_path" -ArgumentList $args -Wait -Passthru
+
+    $args = "update --all --yes"
+    Write-Host $conda_path $args
+    Start-Process -FilePath "$conda_path" -ArgumentList $args -Wait -Passthru
+
+    $args = "clean -tipy"
+    Write-Host $conda_path $args
+    Start-Process -FilePath "$conda_path" -ArgumentList $args -Wait -Passthru
+
+    $args = "install --yes --quiet conda-build anaconda-client jinja2 setuptools"
+    Write-Host $conda_path $args
+    Start-Process -FilePath "$conda_path" -ArgumentList $args -Wait -Passthru
+
+    $args = "clean -tipsy"
     Write-Host $conda_path $args
     Start-Process -FilePath "$conda_path" -ArgumentList $args -Wait -Passthru
 
@@ -78,11 +98,10 @@ function SetUpConda ($Anaconda_cache) {
 
 
 function main () {
-    $Anaconda_VERSION = "5.2.0"
     $Anaconda_cache = "C:\Deps\conda"
     If ($env:ANACONDA_TESTS_ONLY -eq 1) {
-        Write-Host "-- ANACONDA_TESTS_ONLY is set, hence installing version" $Anaconda_VERSION
-        InstallAnaconda $Anaconda_VERSION $Anaconda_cache
+        Write-Host "-- ANACONDA_TESTS_ONLY is set, hence installing latest Miniconda"
+        InstallAnaconda $Anaconda_cache
         SetUpConda $Anaconda_cache
     } Else {
         Write-Host "-- ANACONDA_TESTS_ONLY is not set, hence not installing it!"
