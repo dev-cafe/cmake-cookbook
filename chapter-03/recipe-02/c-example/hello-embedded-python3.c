9282dcd994f348679565f53131354c1555514a29
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-07-24 11:13:45 +0200

    Go back to official Py3 embedding example ch3, r2
    This doesn't double free and doesn't issue warnings with dodgy
    type casts.

--- chapter-03/recipe-02/c-example/hello-embedded-python3.c
+++ chapter-03/recipe-02/c-example/hello-embedded-python3.c
@@ -1,7 +1,7 @@
 #include <Python.h>
 
 int main(int argc, char *argv[]) {
-  wchar_t *program = PyUnicode_DecodeLocale(argv[0], NULL);
+  wchar_t *program = Py_DecodeLocale(argv[0], NULL);
   if (program == NULL) {
     fprintf(stderr, "Fatal error: cannot decode argv[0]\n");
     exit(1);
@@ -10,7 +10,9 @@ int main(int argc, char *argv[]) {
   Py_Initialize();
   PyRun_SimpleString("from time import time,ctime\n"
                      "print('Today is', ctime(time()))\n");
-  Py_Finalize();
+  if (Py_FinalizeEx() < 0) {
+    exit(120);
+  }
   PyMem_RawFree(program);
   return 0;
 }
