0bf0946d7af917bb1e9dcfdfb3ad46a2e0f9e05d
Radovan Bast <bast@users.noreply.github.com>
2018-08-02 13:51:42 +0200

    13/1 different name for C macro variable and CMake variable

--- chapter-13/recipe-01/cxx-example/hello-world.cpp
+++ chapter-13/recipe-01/cxx-example/hello-world.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <string>
 
-const std::string cmake_system_name = CMAKE_SYSTEM_NAME;
+const std::string cmake_system_name = SYSTEM_NAME;
 
 int main() {
   std::cout << "Hello from " << cmake_system_name << std::endl;
