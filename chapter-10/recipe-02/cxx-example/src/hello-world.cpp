7b12461f61bb2e58ecc044902e3c6ce5437155ff
Radovan Bast <bast@users.noreply.github.com>
2018-08-05 22:08:58 +0200

    symlink duplicate files

--- chapter-10/recipe-02/cxx-example/src/hello-world.cpp
+++ chapter-10/recipe-02/cxx-example/src/hello-world.cpp
@@ -1,16 +1,1 @@
-#include <cstdlib>
-#include <iostream>
-
-#include "Message.hpp"
-
-int main() {
-  Message say_hello("Hello, CMake World!");
-
-  std::cout << say_hello << std::endl;
-
-  Message say_goodbye("Goodbye, CMake World");
-
-  std::cout << say_goodbye << std::endl;
-
-  return EXIT_SUCCESS;
-}
+../../../recipe-01/cxx-example/src/hello-world.cpp
