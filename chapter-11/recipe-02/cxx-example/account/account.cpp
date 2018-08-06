1f6ad9a17f0bf8cfeb2841eea25814752f56316f
Radovan Bast <bast@users.noreply.github.com>
2018-08-06 15:42:03 +0200

    11/2 symlink duplicates

--- chapter-11/recipe-02/cxx-example/account/account.cpp
+++ chapter-11/recipe-02/cxx-example/account/account.cpp
@@ -1,11 +1,1 @@
-#include "account.hpp"
-
-Account::Account() { balance = 0.0; }
-
-Account::~Account() {}
-
-void Account::deposit(const double amount) { balance += amount; }
-
-void Account::withdraw(const double amount) { balance -= amount; }
-
-double Account::get_balance() const { return balance; }
+../../../../chapter-09/recipe-05/cxx-example/account/account.cpp
