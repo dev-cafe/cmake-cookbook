90a2c0f6dcdd35250382a409acb118b39ff23ceb
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-08-07 22:32:22 -0400

    Fix ch 14, r 2 for TC pass
    1. Symlink where possible
    2. Add Experimental target to menu

--- chapter-14/recipe-02/cxx-example/tests/test_short.cpp
+++ chapter-14/recipe-02/cxx-example/tests/test_short.cpp
@@ -1,13 +1,1 @@
-#include "sum_integers.hpp"
-
-#include <vector>
-
-int main() {
-  auto integers = {1, 2, 3, 4, 5};
-
-  if (sum_integers(integers) == 15) {
-    return 0;
-  } else {
-    return 1;
-  }
-}
+../../../../chapter-04/recipe-01/cxx-example/test.cpp
