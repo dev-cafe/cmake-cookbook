55342a1ba00998cb4e5e9e6f0014b2125956a933
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-08-11 15:30:39 -0400

    Make Message.hpp in ch 11, r 1 a real file
    CMake won't follow symlink when installing. This commit makes the
    symlink an actual file to avoid being left with an invalid install tree

--- chapter-11/recipe-01/cxx-example/src/Message.hpp
+++ chapter-11/recipe-01/cxx-example/src/Message.hpp
@@ -1,1 +1,21 @@
-../../../../chapter-10/recipe-02/cxx-example/src/Message.hpp
+#pragma once
+
+#include <iosfwd>
+#include <string>
+
+#include "messageExport.h"
+
+class message_EXPORT Message {
+public:
+  Message(const std::string &m) : message_(m) {}
+
+  friend std::ostream &operator<<(std::ostream &os, Message &obj) {
+    return obj.printObject(os);
+  }
+
+private:
+  std::string message_;
+  std::ostream &printObject(std::ostream &os);
+};
+
+std::string getUUID();
