774f6e8f428c7296bf2cc1299988a10307150743
Radovan Bast <bast@users.noreply.github.com>
2018-08-14 15:39:17 +0200

    12/3 symlink repeated files

--- chapter-12/recipe-03/cxx-example/src/Message.cpp
+++ chapter-12/recipe-03/cxx-example/src/Message.cpp
@@ -1,11 +1,1 @@
-#include "Message.hpp"
-
-#include <iostream>
-#include <string>
-
-std::ostream &Message::printObject(std::ostream &os) {
-  os << "This is my very nice message: " << std::endl;
-  os << message_;
-
-  return os;
-}
+../../../recipe-01/cxx-example/src/Message.cpp
