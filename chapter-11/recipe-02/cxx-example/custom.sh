391f31e2a877c0364d19f3381559c554cbadf303
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-08-31 23:20:56 -0400

    Remove gfortran package

--- chapter-11/recipe-02/cxx-example/custom.sh
+++ chapter-11/recipe-02/cxx-example/custom.sh
@@ -7,12 +7,14 @@ if [ $# -eq 0 ] ; then
     exit 1
 fi
 
-# Remove symlinks
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-find "$script_dir" -type l -exec sh -c '
-    file=$(basename "$1")
-    directory=${1%/*}
-    (cd "$directory" && cp --remove-destination "$(readlink "$file")" "$file")' sh {} ';'
+# Remove symlinks if on CircleCI with Docker
+if [ -n "${CIRCLECI_COMPILER+x}" ]; then
+    script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+    find "$script_dir" -type l -exec sh -c '
+      file=$(basename "$1")
+      directory=${1%/*}
+      (cd "$directory" && cp --remove-destination "$(readlink "$file")" "$file")' sh {} ';'
+fi
 
 # build directory is provided by the main script
 build_directory="$1"
